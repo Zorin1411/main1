45bae2d5bd3dc69bf7d20d9dd55d50920fed2bea
Dmitry <dima_zorin_03@mail.ru>
2023-05-19 11:50:37 +0600

    Добавлена возможность вычитания чисел

--- file.c
+++ file.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main() {
-   int a = 5;
-   int b = 3;
-   int sum = a + b; 
-   printf("Сумма: %d\n", sum); 
-   return 0;
+    int a = 5;
+   	int b = 3;
+	int sum = a + b;
+	int difference = a - b; 
+	printf("Сумма: %d\n", sum); 
+	printf("Разность: %d\n", difference); 
+	return 0;
 }
